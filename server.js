d8465a6505a62511d8f876e6b85d8897fc9a242a
Lauren Morrow <morrowlauren17@gmail.com>
2018-09-23 13:30:42 -0700

    added recipes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 // we import the ShoppingList model, which we'll
 // interact with in our GET endpoint
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -21,6 +22,7 @@ app.use(morgan('common'));
 // normally you wouldn't do this. Usually your
 // server will simply expose the state of the
 // underlying database.
+
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
@@ -29,6 +31,14 @@ ShoppingList.create('peppers', 4);
 // all current ShoppingList items by calling `ShoppingList.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('mashed potatoes', ['potatoes', 'milk', 'butter', 'salt', 'sour cream']);
+Recipes.create('scrambeld eggs', ['eggs', 'water', 'cheese', 'salt', 'pepper', 'green onion']);
+
+app.get('/recipes', (req, res) => {
+	res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
